c86df17338d4621d6755eac4d9e22e6c28894b98
lChap701 <lucas.chapman@stu.indianhills.edu>
2020-10-04 16:43:02 -0500

    Modules for connecting to the database and encrypting data

---
+++ connectlib.py
@@ -0,0 +1,37 @@
+import mysql.connector, sys
+from mysql.connector import Error
+
+# Custom Python module for connecting to the "Vote" database
+
+
+def connect_db():
+    """
+    Connects to the "Vote" database
+    """
+    try:
+        conn = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="",
+            database="vote",
+        )
+        return conn
+
+    except Error as e:
+        print("<!DOCTYPE html5>")
+        print('<html lang="en">')
+        print("  <head>")
+        print("    <title>Database Eror</title>")
+        print("  </head>")
+        print("  <body>")
+        print("    <h1>Error: Unable to connect to the database</h1>")
+        print(
+            "    <p>Please wait until this issue is resolved before visiting this website</p>"
+        )
+
+        # Displays the error that occured
+        print("    <script>console.log('", e, "')</script>")
+
+        print("  </body>")
+        print("</html>")
+        sys.exit()  # ends the Python script early
