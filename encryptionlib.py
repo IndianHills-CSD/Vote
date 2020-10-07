ae17ffc9dcefa29fcaef5aa103512648920181fa
lChap701 <lucas.chapman@stu.indianhills.edu>
2020-10-07 13:22:39 -0500

    Added more comments

--- encryptionlib.py
+++ encryptionlib.py
@@ -7,6 +7,7 @@ def create_hash(data, salt):
     """
     Encrypts the data
     """
+    # Adds the salt (in hex) to the data that was entered
     salt_input = str(data) + str(salt.hex())
     hashed = hashlib.pbkdf2_hmac(
         "sha224", salt_input.encode("ascii"), salt, 10000
@@ -18,6 +19,7 @@ def verify_hash(hashed, data, salt):
     """
     Checks to see if the hash matches the unencrypted data
     """
+    # Adds the salt (in hex) to the data that was entered
     salt_input = str(data) + str(salt.hex())
     new_hash = hashlib.pbkdf2_hmac(
         "sha224", salt_input.encode("ascii"), salt, 10000
